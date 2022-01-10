f1f1f0bdbf8d0f0de2139ca3fb8251fd408f641f
Anish1411 <85382626+Anish1411@users.noreply.github.com>
2022-01-10 10:46:16 +0530

    P-1/D-1

---
+++ Peek element using Binery search.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        int s=0;
+        int mid;
+        int e=a.size()-1;
+        while(s<e){
+            mid=(s+(e-s)/2);
+            if(a[mid]<a[mid+1] && mid+1<a.size()){
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+        }
+        return a[s];
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+  // } Driver Code Ends
